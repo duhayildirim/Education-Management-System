c72d89a001236af2fa003b75f0bda809c37f4a9b
Duha YILDIRIM <1duhayildirim@gmail.com>
2023-02-23 02:57:14 +0300

    app screen location center helper

--- src/com/educationWebsite/Helper/Helper.java
+++ src/com/educationWebsite/Helper/Helper.java
@@ -1,5 +1,20 @@
 package com.educationWebsite.Helper;
 
-public class Helper {
+import java.awt.*;
 
+public class Helper {
+    public static int appScreenLocationCenter(String point, Dimension size) {
+        int flat;
+        switch (point) {
+            case "x":
+                flat = (Toolkit.getDefaultToolkit().getScreenSize().width - size.width) / 2;
+                break;
+            case "y":
+                flat = (Toolkit.getDefaultToolkit().getScreenSize().height - size.height) / 2;
+                break;
+            default:
+                flat  = 0;
+        }
+        return flat;
+    }
 }
