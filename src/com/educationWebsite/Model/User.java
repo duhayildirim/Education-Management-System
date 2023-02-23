54693794599527f0aa8ea2f592b13fa288062c64
Duha YILDIRIM <1duhayildirim@gmail.com>
2023-02-23 05:19:26 +0300

    get user

--- src/com/educationWebsite/Model/User.java
+++ src/com/educationWebsite/Model/User.java
@@ -68,14 +68,23 @@ public class User {
     public static ArrayList<User> getList() {
         ArrayList<User> userList = new ArrayList<>();
         String query = "SELECT * FROM user";
+        User obj;
         try {
             Statement st = DBConnecter.getInstance().createStatement();
             ResultSet rs = st.executeQuery(query);
             while (rs.next()) {
+                obj = new User();
+                obj.setId(rs.getInt("id"));
+                obj.setName(rs.getString("name"));
+                obj.setUsername(rs.getString("username"));
+                obj.setPassword(rs.getString("password"));
+                obj.setRole(rs.getString("role"));
 
+                userList.add(obj);
             }
         } catch (SQLException e) {
             throw new RuntimeException(e);
         }
+        return userList;
     }
 }
